694ab61d53085dc64b8c367d566ae9451718d3bd
dieegobs <diegohevant@gmail.com>
2023-03-14 23:28:18 -0300

    Imagem Interna

--- App.tsx
+++ App.tsx
@@ -10,8 +10,14 @@ const App = () => {
      <Image
         source={{uri: 'https://www.google.com.br/google.jpg'}}
         style={{width: 300, height: 100}}
+      />
+
+    <Image
+        source={require('./assets/image.jpg')}
+        style={{width: 500, height: 500}}
       
       />
+
 
     </View>  
   );
